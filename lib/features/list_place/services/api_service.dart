ef3e17b7353d7021e7e168954db53f77fba034f5
Tatyana Samoshkina <73110438+ingvilow@users.noreply.github.com>
2022-06-09 12:15:50 +0300

    Update api_service.dart

--- lib/features/list_place/services/api_service.dart
+++ lib/features/list_place/services/api_service.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:test_case/features/list_place/models/places.dart';
 
 /// класс для работы с запросами к АПИ
-class ApiService {
+class PlaceApiService {
   /// выдает список всех имеющтхся мест, т.к их очень много, то пока выдает 15 штук
   /// offset - необходим для реализации пагинации
   Future<List<Places>> fetchPlaces(int offset) async {
