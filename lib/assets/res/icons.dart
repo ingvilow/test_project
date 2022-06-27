cbf7218b997cea1b01a69426644d208adc809fde
aedthuill <samosh3223@gmail.com>
2022-06-27 11:28:11 +0300

    Revert "SPO-13135 - 1) выводит список мест 2) ошибку при выполнении запроса, состояние загрузки. 3) добавлена возможность перезагрузить страницу при нажатии на кнопку. 4)Во время перезагрузки появляется лоадер."
    This reverts commit 7ae1466d0b94a154867162f79895afed9da83413.

--- lib/assets/res/icons.dart
+++
@@ -1,5 +0,0 @@
-/// иконки приложения
-class GuideIcons {
-  static const String errorIcon = 'assets/icons/error.png';
-  static const String loaderLarge = 'assets/icons/large_loader.png';
-}
--- lib/assets/themes/text_style.dart
+++ lib/assets/themes/text_style.dart
@@ -1,70 +1,14 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
-import 'package:flutter/services.dart';
 import 'package:test_case/assets/colors/colors.dart';
 
 //ignore_for_file: public_member_api_docs
 
 /// Styles of texts.
 
-const TextStyle titleBig = TextStyle(
-  fontWeight: FontWeight.w500,
-  color: ColorTypography.typographyPrimary,
-  fontSize: 16,
-);
-
-const TextStyle titleSmall = TextStyle(
-  fontWeight: FontWeight.w400,
-  color: ColorTypography.typographySecondary,
-  fontSize: 14,
-);
-
-const TextStyle titleSmaller = TextStyle(
-  fontWeight: FontWeight.w700,
-  color: ColorTypography.typographyTertiary,
-  fontSize: 14,
-);
-
-const TextStyle appBarMainScreen = TextStyle(
-  fontWeight: FontWeight.w700,
-  color: ColorTypography.typographyPrimary,
-  fontSize: 32,
-);
-
 const TextStyle _text = TextStyle(
   fontStyle: FontStyle.normal,
   color: textColorPrimary,
 );
-
-const TextStyle bigTextErrorStyle = TextStyle(
-  fontWeight: FontWeight.w500,
-  fontSize: 18,
-  color: ColorTypography.errorTextColor,
-);
-
-const TextStyle smallTextErrorStyle = TextStyle(
-  fontWeight: FontWeight.w400,
-  fontSize: 14,
-  color: ColorTypography.typographyTertiary,
-);
-
-/// тема приложения
-class AppTheme {
-  static const appBarTheme = AppBarTheme(
-    backgroundColor: ColorTypography.typographyTertiary,
-    elevation: 0,
-    systemOverlayStyle: SystemUiOverlayStyle(
-      statusBarIconBrightness: Brightness.dark,
-      statusBarColor: ColorTypography.typographyTertiary,
-    ),
-  );
-
-  static ThemeData get defaultTheme => ThemeData(
-        backgroundColor: ColorTypography.typographyTertiary,
-        appBarTheme: appBarTheme,
-        scaffoldBackgroundColor: ColorTypography.typographyTertiary,
-      );
-}
 
 //Light
 TextStyle textLight = _text.copyWith(fontWeight: FontWeight.w300);
